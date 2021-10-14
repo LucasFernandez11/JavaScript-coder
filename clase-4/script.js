a4e4c382b3c19f7ae8748aecf099a5856ae362d3
Lucas Fernández <89951452+LucasFernandez11@users.noreply.github.com>
2021-10-14 14:15:25 -0300

    clase 4

--- clase-4/script.js
+++ clase-4/script.js
@@ -1,1 +1,18 @@
 //funciones 
+const primos = (num) =>{
+    let resultado;
+    let contador=0;
+    for (let i = 1; i <= num; i++) {
+        if(num % i == 0){
+            contador ++;
+        }
+    }
+    if (contador == 2) {
+        resultado = true;
+    } else {
+        resultado = false;
+    }
+    return resultado;
+}
+
+console.log(primos(num));
